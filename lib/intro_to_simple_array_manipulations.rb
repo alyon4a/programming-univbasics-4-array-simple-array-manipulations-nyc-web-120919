6b8d20efa154ec229ab314b6b13a9da6b48072d9
Olena Matokhina <olena@matokhina.com>
2019-11-17 17:59:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,10 +38,10 @@ def using_flatten (array)
   array.flatten
 end
 
-def using_delete
-  
+def using_delete (array, string)
+  array.delete(string)
 end
 
-def using_delete_at
-  
+def using_delete_at (array, number)
+  array.delete(number)
 end
